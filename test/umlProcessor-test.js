b3bf29938e37fea532322622eac303f75a662e4e
Stefan Schöffmann <stefan.schoeffmann@posteo.de>
2015-06-10 06:55:45 +0200

    Added a new File under test

--- test/umlProcessor-test.js
+++ test/umlProcessor-test.js
@@ -7,8 +7,11 @@ var fs = require('fs');
 var _ = require('lodash');
 
 describe("'umlProcessor'", function() {
+    /*
+     * this one does not run on travis-ci (needs investigation)
+     */
     it('should turn a svg to a png using convert (no matter if convert produces good results)', function (done) {
-        fs.readFile('test/testFixtures/class.svg', {encoding: 'utf-8'}, function(err, inputData) {
+        fs.readFile('test/testFixture3.svg', {encoding: 'utf-8'}, function(err, inputData) {
             umlProcessor.transformToPNG(inputData, function (result) {
                 assert(Buffer.isBuffer(result));
                 assert(Buffer.isEncoding('binary'));
